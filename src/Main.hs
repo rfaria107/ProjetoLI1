e582bbf6d46732bae07ee79f799d99d990178649
Octávio Henriques <a104277@alunos.uminho.pt>
2023-11-28 20:50:24 +0000

    Desenho do jogador (Teste)

--- src/Main.hs
+++ src/Main.hs
@@ -42,11 +42,14 @@ fazLinha :: [Bloco] -> Int -> Int -> [(Bloco,(Int,Int))]
 fazLinha [] _ _ = []
 fazLinha (bloco:rblocos) x y = (bloco, (x,y)) : fazLinha rblocos (x+l) y
 
+desenhaJogador :: Imagens -> Personagem -> Picture
+desenhaJogador images (Personagem _ Jogador (x,y) direcao tamanho _ _ _ _ _) =
+        Translate (-440) (-310) $ Scale 0.20 0.20 $ fromJust $ lookup "Jogador" images
+
 
 desenhaMapa :: EstadoMapa -> [[(Bloco,(Int,Int))]] -> Picture
 desenhaMapa _ [] = blank
 desenhaMapa estadogloss@((Jogo mapa _ _ _), images) (h:t) = pictures [desenhaLinhaMapa images h, desenhaMapa estadogloss t]
-
 
 desenhaLinhaMapa :: Imagens -> [(Bloco, (Int,Int))] -> Picture
 desenhaLinhaMapa _ [] = blank
@@ -56,10 +59,12 @@ desenhaLinhaMapa images ((h,(x,y)):t) = case h of
             V -> Pictures [Translate ((fromIntegral x)*(realToFrac l)) (-(fromIntegral y)*(realToFrac l)) $ Scale 0.3 0.3 $ fromJust $ lookup "Vazio" images, desenhaLinhaMapa images t ]
             A -> Pictures [Translate ((fromIntegral x)*(realToFrac l)) (-(fromIntegral y)*(realToFrac l)) $ Scale 1 1 $ fromJust $ lookup "Alcapao" images , desenhaLinhaMapa images t ]
 
-desenhaEstado :: Imagens -> Jogo -> Picture
-desenhaEstado images jogo@(Jogo mapa@(Mapa (posi,dir) posf blocos) inimigos colecionaveis jogador) = pictures [Color black $ rectangleSolid 1200 900,
-                                                                                                                desenhaMapa (jogo,images) (fazMatriz blocos (-20))]
 
+desenhaEstado :: Imagens -> Jogo -> Picture
+desenhaEstado images jogo@(Jogo mapa@(Mapa (posi,dir) posf blocos) inimigos colecionaveis jogador) =
+  pictures [Color black $ rectangleSolid 1200 900,
+            desenhaMapa (jogo, images) (fazMatriz blocos (-20)),
+            desenhaJogador images jogador]
 
 fr :: Int 
 fr = 60
@@ -69,10 +74,12 @@ carregarImagens = do
         plataforma <- loadBMP "../2023li1g086/src/block.bmp"
         escadas <- loadBMP "../2023li1g086/src/Ladder.bmp"
         alcapao <- loadBMP "../2023li1g086/src/alcapao.bmp"
+        jogador <- loadBMP "../2023li1g086/src/player.bmp"
         let imagens = [  ("Plataforma", scale 0.43 0.2 $ plataforma),
                           ("Escada", scale 0.43 0.5 $ escadas),
-                          ("Alcapao", scale 0.43 0.43 $ alcapao),
-                          ("Vazio", Blank)
+                          ("Alcapao", scale 0.52 0.43 $ alcapao),
+                          ("Vazio", Blank),
+                          ("Jogador", scale 0.3 0.3 $ jogador)
                         ]
           
         return imagens
@@ -81,7 +88,7 @@ main = do
         images <- carregarImagens 
         play
             FullScreen                         
-            yellow            
+            yellow               
             50                        
             (estadoInicial)
             (desenhaEstado images)        
