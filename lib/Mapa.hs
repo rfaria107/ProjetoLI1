b4f1a4ea02022ef3f2b05763cbde4fd2c7f154ac
Octávio Henriques <a104277@alunos.uminho.pt>
2024-01-09 20:11:44 +0000

    Mapa levemente mudado(um bloco .-.)

--- lib/Mapa.hs
+++ lib/Mapa.hs
@@ -7,7 +7,7 @@ mapa1 = Mapa ((0.5, 5.5), Oeste) (0.5, 2.5)
         [[p, p, p, p, p, p, p, p, p, p]
         ,[v, v, v, v, v, v, v, v, v, v]
         ,[v, v, v, v, v, v, v, v, v, v]
-        ,[p, p, a, p, v, v, p, p, p, p]
+        ,[p, p, a, p, v, p, p, p, p, p]
         ,[v, v, v, v, v, v, v, v, e, v]
         ,[v, v, e, v, v, v, v, v, e, v]
         ,[p, p, p, p, p, p, p, p, p, p]
